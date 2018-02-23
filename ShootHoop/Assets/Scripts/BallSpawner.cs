3978649c836c22783eb64dc610dd9fd35bd7aefc
hakbar0 <haseeb2000@hotmail.com>
2018-02-23 18:38:20 +0000

    Able to Instantiate basketballs.

---
+++ ShootHoop/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+	public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))	Instantiate (ballPrefab);
+			}
+}
