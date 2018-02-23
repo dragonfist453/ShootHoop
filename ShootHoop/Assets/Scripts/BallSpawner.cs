fd5b8310cd70cbbabc2637d077c761ba34d92872
hakbar0 <haseeb2000@hotmail.com>
2018-02-23 18:47:37 +0000

    Able to spawn balls with different veloicty. Need to use the DRY principle.

--- ShootHoop/Assets/Scripts/BallSpawner.cs
+++ ShootHoop/Assets/Scripts/BallSpawner.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallSpawner : MonoBehaviour {
+public class BallSpawner : MonoBehaviour
+{
 
 	public GameObject ballPrefab;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown(KeyCode.Space))	Instantiate (ballPrefab);
-			}
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			GameObject instance = Instantiate (ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody> ();
+			rb.velocity = new Vector3 (-5, 0, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			GameObject instance = Instantiate (ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody> ();
+			rb.velocity = new Vector3 (5, 0, 0);
+		}
+	}
+
 }
