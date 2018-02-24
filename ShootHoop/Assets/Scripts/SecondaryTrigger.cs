fb25c3655b0bc6de06f19fa4f51a63d2b3b00fe8
hakbar0 <haseeb2000@hotmail.com>
2018-02-24 14:43:11 +0000

    Added Audio to scenes in the game. Added new colliders for the basket. Added baskets to the scene.

---
+++ ShootHoop/Assets/Scripts/SecondaryTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondaryTrigger : MonoBehaviour {
+
+	Collider expectedCollider;
+
+	public void ExpectCollider(Collider collider){
+		expectedCollider = collider;
+	}
+
+	void OnTriggerEnter(Collider otherCollider){
+		if(otherCollider == expectedCollider) {
+			ScoreKeeper scoreKeeper = FindObjectOfType<ScoreKeeper>();
+			scoreKeeper.IncrementScore (1);
+		}
+	}
+
+}
